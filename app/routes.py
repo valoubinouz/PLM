45a6003ebef66a09db2753b90e1733b0f82f2573
valoubinouz <vbertogliati@gmail.com>
2024-12-13 19:45:47 +0100

    push miteux login a bosser

--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,9 @@ def load_product():
     with open('app/data/product.json', 'r') as f:
         return json.load(f)
 
-def save_product(data):
+def save_product(products):
     with open('app/data/product.json', 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(products, f, indent=4)
 
 def init_routes(app):
     @app.route('/')
@@ -28,31 +28,35 @@ def init_routes(app):
 
 # Charger les utilisateurs depuis le fichier JSON
 def load_users():
-    with open('app/data.json', 'r') as f:
+    with open('app/data/user.json', 'r') as f:
         data = json.load(f)
     return data.get('users', [])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
         users = load_users()
 
         # Vérifier si l'utilisateur existe
+        """"
         user = next((u for u in users if u['username'] == username and u['password'] == password), None)
+        
         if user:
             session['username'] = user['username']
             session['role'] = user['role']
-
-            # Rediriger selon le rôle
-            if user['role'] == 'admin':
+        """
+            
+# Rediriger selon le rôle
+    if username['role'] == 'admin':
                 return redirect(url_for('admin_dashboard'))
-            elif user['role'] == 'client':
+    elif username['role'] == 'client':
                 return redirect(url_for('client_dashboard'))
 
         # En cas d'échec
-        return render_template('login.html', error="Invalid credentials")
+    return render_template('login.html', error="Invalid credentials")
 
     return render_template('login.html')
 
@@ -61,13 +65,13 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET'])
 def admin_dashboard():
     if 'role' in session and session['role'] == 'admin':
         return render_template('admin_dashboard.html')
     return redirect(url_for('login'))
 
-@app.route('/client')
+@app.route('/client', methods=['GET'])
 def client_dashboard():
     if 'role' in session and session['role'] == 'client':
         return render_template('client_dashboard.html')
